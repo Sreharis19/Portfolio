d66901f6dc01f3db35a349eb89a4ed5cf1a6d1e5
sreeharis19 <sreeharis19@gmail.com>
2024-09-10 16:00:57 +0100

    image update

--- constants.ts
+++ constants.ts
@@ -180,7 +180,7 @@ export const TIMELINE: Array<TimelineNodeV2> = [
     size: ItemSize.SMALL,
     subtitle: "Led the development of Ensource, a FinTech hybrid application, improving transaction efficiency by 30%.",
     image: "/company/reubro.svg",
-    slideImage: "/timeline/raw/Ensource.png",
+    slideImage: "/timeline/raw/Ensource2.png",
     shouldDrawLine: true,
     alignment: Branch.LEFT,
   }, {
